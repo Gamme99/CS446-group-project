36817c1af8762c6da695ec92290353bf62f7619a
Galmo <galmogalamso@gmail.com>
2022-05-24 19:04:12 -0700

    Adding Banner files

--- src/Banner.js
+++ src/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className="banner">
       <div className="banner-info">
-        <h1>Where do you want to go?</h1>
+        <h1>Where do you want to go? </h1>
         <input type="text" className="textbox" />
         <button className="button">GO</button>
       </div>
