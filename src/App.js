ba5425475839a2e62acdf7e0b83d342ded0216ca
Logan H <halversonL@cwu.edu>
2022-05-25 23:43:23 -0700

    Navbar Improvement

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import "./css/App.css";
-import NavBar from "./NavBar";
-import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./css/App.css";
+
+import NavBar from "./NavBar";
 import Rent from "./pages/Rent";
 import Landing from "./pages/Landing";
 import Property from "./pages/Property";
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,27 +1,36 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import { Nav } from "react-bootstrap";
+import { Nav, Navbar, NavbarBrand, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
 	return (
-		<div>
-			<ul>
-				<Nav>
-					<Nav.Item>
-						<Link to="./">Landing</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link to="./rent">Rent</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link to="./property">Property</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link to="./about">About</Link>
-					</Nav.Item>
-				</Nav>
-			</ul>
-		</div>
+		<Navbar bg="light">
+			<Link to="./">
+				<NavbarBrand
+					style={{
+						marginRight: "20px",
+						marginLeft: "40px",
+						fontWeight: "bold",
+					}}
+				>
+					Residence Finder
+				</NavbarBrand>
+			</Link>
+
+			<Stack direction="horizontal" gap={4}>
+				<Nav.Item>
+					<Link to="./">Landing</Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Link to="./rent">Rent</Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Link to="./property">Property</Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Link to="./about">About</Link>
+				</Nav.Item>
+			</Stack>
+		</Navbar>
 	);
 }
