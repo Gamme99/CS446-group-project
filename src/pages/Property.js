6890504c35d3aa72358c6268dbab62ffe840a027
Logan H <halversonL@cwu.edu>
2022-05-25 23:51:06 -0700

    Property Page Instantiation

--- src/pages/Property.js
+++ src/pages/Property.js
@@ -1,9 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Carousel, Container } from "react-bootstrap";
+import "../css/Property.css";
 
 export default class Property extends Component {
-  render() {
-    return (
-      <div>Property</div>
-    )
-  }
+	render() {
+		return (
+			<Container>
+				<Carousel variant="dark">
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src="https://i.imgur.com/flFwYUG.png"
+							alt="First slide"
+						/>
+					</Carousel.Item>
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src="https://i.imgur.com/xyAmMwH.png"
+							alt="Second slide"
+						/>
+					</Carousel.Item>
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src="https://i.imgur.com/Cms0ccg.png"
+							alt="Third slide"
+						/>
+					</Carousel.Item>
+				</Carousel>
+				<h1 className="hello">hello</h1>
+			</Container>
+		);
+	}
 }
