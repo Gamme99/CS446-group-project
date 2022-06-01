3152f10d03531ff695a5926f7fa8e29500828015
Logan H <halversonL@cwu.edu>
2022-05-31 21:32:05 -0700

    Created About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Container, Col } from "react-bootstrap";
+import "../css/About.css";
 
 export default function About() {
 	return (
 		<div>
-			{" "}
-			<h1>Hello.</h1>
+			<Container>
+				<Col md={{ span: 6, offset: 3 }} id="col">
+					<h1 className="title">Residence Finder</h1>
+
+					<p className="body">
+						Created for the class CS446 at Central Washington University. The{" "}
+						<a href="https://github.com/Gamme99/CS446-group-project">
+							Github Repo
+						</a>{" "}
+						can be found at the link.
+					</p>
+				</Col>
+			</Container>
 		</div>
 	);
 }
