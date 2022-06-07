8ea13540e561f48345f6baa04194826b3ebd79e0
Galmo <galmogalamso@gmail.com>
2022-06-07 12:23:04 -0700

    changed link from rent to listing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../css/Banner.css";
-
+import { Link } from "react-router-dom";
 export default function Landing() {
+<<<<<<< Updated upstream
 	return (
 		<div className="banner">
 			<div className="banner-info">
@@ -16,4 +17,19 @@ export default function Landing() {
 			</div>
 		</div>
 	);
+=======
+  return (
+    <div className="banner">
+      <div className="banner-info">
+        <h1>Where do you want to go? </h1>
+        <input type="text" className="textbox" />
+        <Link to="./listings">
+          <button className="button" href="./Rent">
+            GO
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
